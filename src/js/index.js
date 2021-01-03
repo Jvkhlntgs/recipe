ae8df6b0bb8f5f62d5cf1b1046b39df7a156a9fb
Jvkhlntgs <javkhaani@gmail.com>
2021-01-03 12:52:57 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 import Search from "./model/search";
 
-let search = new Search("pasta");
+/**
+ * Web app tulu
+ * Hailtiin query, ur dun
+ * Tuhain uzuulj bui jor
+ * Likelsan joruud
+ * Zahialj baigaa joriin nairlaguud
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controllSearch = async () => {
+  //1. Webees hailtiin tulhuur ugiig avna
+  const query = "pizza";
+
+  if (query) {
+    //2.Shineer hailtiin object uusgej ogno
+    state.search = new Search(query);
+    //3.Hailt hiihed zoriuljdelgetsiin ui beldene
+    //4.Hailtiig guitsetgene
+    await state.search.doSearch();
+    //5.Hailtiin ur dung delgetsend uzuulne
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controllSearch();
+});
